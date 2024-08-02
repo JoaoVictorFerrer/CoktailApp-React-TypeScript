4e1b36bee51d33b94dd97cf8af7eaf53e66b4d1e
JoaoVictorFerrer <137827716+JoaoVictorFerrer@users.noreply.github.com>
2024-08-02 11:38:25 +0200

    aumentando la perfomance de las rutas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,26 @@
+import {lazy,Suspense} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from "./views/IndexPage";
-import FavoritesPage from "./views/FavoritesPage";
 import Layout from "./layouts/Layout";
+const FavoritesPage = lazy(()=> import('./views/FavoritesPage'))
+const IndexPage = lazy(()=> import('./views/IndexPage'))
 
+
+//! Aumentamos la performace de la pagina fragamentando las rutas yq eu se carge la parte correspondiente en cada llamada
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout/>}>
-          <Route path="/" element={<IndexPage />} index />
-          <Route path="/favoritos" element={<FavoritesPage />} />
+          <Route path="/" element={
+            <Suspense fallback='Cargando....'> {/* en la prop fallback puedo renderezar un loading mientras carga la pagina */}
+               <IndexPage/>
+           </Suspense>
+          } index />
+          <Route path="/favoritos" element={
+            <Suspense fallback='Cargando....'>
+              <FavoritesPage/>
+            </Suspense>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
